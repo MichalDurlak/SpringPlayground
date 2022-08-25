8e0769838a900b09fc5e9937391cd768b9b2c134
Michał Durlak <durlak.m@wp.pl>
2022-08-25 18:47:19 +0200

    ComponentScan
    Learning how to scan other packages

--- src/main/java/pl/michaldurlak/SpringPlayground/Starter.java
+++ src/main/java/pl/michaldurlak/SpringPlayground/Starter.java
@@ -1,12 +1,22 @@
 package pl.michaldurlak.SpringPlayground;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+import pl.michaldurlak.SpringPlayground.domain.Quest;
 import pl.michaldurlak.SpringPlayground.domain.Student;
+import pl.michaldurlak.SpringPlayground.domain.University;
+import pl.michaldurlak.component.TestComponent;
 
 @Component
+//@ComponentScan({"pl.michaldurlak.SpringPlayground","pl.michaldurlak.component"})
+@ComponentScan(basePackageClasses = {Starter.class, University.class, Quest.class, TestComponent.class})
 public class Starter implements CommandLineRunner {
 
+    @Autowired
+    TestComponent testComponent;
+    // aplikacja nie wystartuje bez dodania ComponentScan, ponieważ nie bedzie moglo wstrzyknac testComponent bo nie odnajdzie classy
 
     @Override
     public void run(String... args) throws Exception {

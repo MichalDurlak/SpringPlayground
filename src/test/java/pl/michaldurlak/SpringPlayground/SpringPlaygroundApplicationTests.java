20129747860b3b4218d753cc6818cf9ee51c9bef
Michał Durlak <durlak.m@wp.pl>
2022-08-25 18:32:15 +0200

    First basic test using spring

--- src/test/java/pl/michaldurlak/SpringPlayground/SpringPlaygroundApplicationTests.java
+++ src/test/java/pl/michaldurlak/SpringPlayground/SpringPlaygroundApplicationTests.java
@@ -2,6 +2,9 @@ package pl.michaldurlak.SpringPlayground;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.michaldurlak.SpringPlayground.domain.Student;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringPlaygroundApplicationTests {
@@ -9,5 +12,13 @@ class SpringPlaygroundApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void testStudentToString(){
+		String response = "Student{name='Andrzej', age=18, quest=Quest{description='Zdaj matematyke'}}";
+		Student student = new Student("Andrzej",18);
+		assertEquals(response,student.toString());
+	}
+	// testy z użyciem springa są bardzo ciężkie i trwają długo
 
 }

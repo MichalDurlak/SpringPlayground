ca72fbc2d4d4d56fbdb8a8b74f37a3977982b0e4
Michał Durlak <durlak.m@wp.pl>
2022-08-25 07:15:15 +0200

    Repo init
    Send basic files from spring project

---
+++ src/test/java/pl/michaldurlak/SpringPlayground/SpringPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michaldurlak.SpringPlayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
